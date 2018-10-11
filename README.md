0a288075a3c95efff3c2d17200ee186ea6b89e04
Marek <marekficht@gmail.com>
2018-10-11 19:35:46 +0200

    First structure. First home screen.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Portfolio page in new style!
+
+[DEMO]()
+
+The goal was to create a different page than all. <br/>
+We will get to individual sections by the tiles
+
+RWD comming soon.
+
+### Installation and configuration
+
+You need nothing install, to see the effects. You can open this project in:  `build/index.html`
+
+If u want to make changes, then must: 
+- instal Node.js -> [NodeJS](https://nodejs.org/en/)
+- open folder with all files, and open your command prompt.<br/> 
+Next: 
+- If you want create build: `npm run build`
+- If you want to view changes live: `npm start` OR `npm run start`
+
+Works on browsers: `Chrome`, `Opera`.
+
+## Built of ...
+
+- [Webpack](https://webpack.js.org/)
+- [JavaScript](https://developer.mozilla.org/pl/docs/Web/JavaScript)
+- [SASS](https://sass-lang.com/)
+- [CSS](https://developer.mozilla.org/pl/docs/Web/CSS)
+- [HTML](https://developer.mozilla.org/pl/docs/Web/HTML)
---
+++ app.js
@@ -0,0 +1,53 @@
+import 'jquery';
+import './main.scss';
+
+$(document).ready( function () {
+    console.log('%c ------ DOM loaded ------', 'color: green; font-weight: bold;');
+
+    const leftTop = $('.left-top');
+    const leftBot = $('.left-bottom');
+    const rightTop = $('.right-top');
+    const rightBot = $('.right-bottom');
+
+    const btnStart = $('#btn-start');
+    const content = $('.content');
+
+    btnStart.click( function () {
+
+        if (leftTop.hasClass('left-top-move')) {
+
+            leftTop.removeClass('left-top-move');
+            leftTop.children('span').removeClass('animationCube');
+
+            rightTop.removeClass('right-top-move');
+            rightTop.children('span').removeClass('animationCube');
+
+            leftBot.removeClass('left-bottom-move');
+            leftBot.children('span').removeClass('animationCube');
+
+            rightBot.removeClass('right-bottom-move');
+            rightBot.children('span').removeClass('animationCube');
+
+            content.removeClass('animationContent');
+        }
+        else {
+
+            leftTop.addClass('left-top-move');
+            leftTop.children('span').addClass('animationCube');
+
+            rightTop.addClass('right-top-move');
+            rightTop.children('span').addClass('animationCube');
+
+            leftBot.addClass('left-bottom-move');
+            leftBot.children('span').addClass('animationCube');
+
+            rightBot.addClass('right-bottom-move');
+            rightBot.children('span').addClass('animationCube');
+
+            content.addClass('animationContent');
+        }
+
+
+    } )
+
+})
