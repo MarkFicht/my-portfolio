4245ebb3c3a92ff24fbb57c39a2417b258774154
Marek <marekficht@gmail.com>
2018-10-12 22:25:14 +0200

    Fixed btns and borders. Cleared SASS.

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ $(document).ready( function () {
     console.log('%c ------ DOM loaded ------', 'color: green; font-weight: bold;');
 
     const leftTop = $('.left-top');
+    const btnOMnie = $('#submenu-me');
     const leftBot = $('.left-bottom');
     const rightTop = $('.right-top');
     const rightBot = $('.right-bottom');
 
     const btnStart = $('#btn-start');
     const content = $('.content');
+
+    const btnBack = $('#btn-back');
 
     btnStart.click( function () {
 
@@ -20,40 +23,56 @@ $(document).ready( function () {
             $('.container-content').css('box-shadow', '0 0 20px black');
 
             leftTop.removeClass('left-top-move');
-            leftTop.children('span').removeClass('animationCube');
+            leftTop.children('span').removeClass('visibility');
 
             rightTop.removeClass('right-top-move');
-            rightTop.children('span').removeClass('animationCube');
+            rightTop.children('span').removeClass('visibility');
 
             leftBot.removeClass('left-bottom-move');
-            leftBot.children('span').removeClass('animationCube');
+            leftBot.children('span').removeClass('visibility');
 
             rightBot.removeClass('right-bottom-move');
-            rightBot.children('span').removeClass('animationCube');
+            rightBot.children('span').removeClass('visibility');
 
-            content.removeClass('animationContent');
+            content.removeClass('visibilityContent');
         }
         else {
+
+            let cubesMove = setTimeout( function () {
+                leftTop.addClass('left-top-move');
+                rightTop.addClass('right-top-move');
+                leftBot.addClass('left-bottom-move');
+                rightBot.addClass('right-bottom-move');
+            }, 1100);
 
             $('.blur').css('filter', 'blur(4px)');
             $('.container-content').css('box-shadow', '0 0 50px black, 0 0 30px black');
 
-            leftTop.addClass('left-top-move');
-            leftTop.children('span').addClass('animationCube');
-
-            rightTop.addClass('right-top-move');
-            rightTop.children('span').addClass('animationCube');
-
-            leftBot.addClass('left-bottom-move');
-            leftBot.children('span').addClass('animationCube');
-
-            rightBot.addClass('right-bottom-move');
-            rightBot.children('span').addClass('animationCube');
-
-            content.addClass('animationContent');
+            leftTop.children('span').addClass('visibility');
+            rightTop.children('span').addClass('visibility');
+            leftBot.children('span').addClass('visibility');
+            rightBot.children('span').addClass('visibility');
+            content.addClass('visibilityContent');
         }
+    })
+
+    btnOMnie.click( function () {
+
+        leftTop.removeClass('left-top-move');
+        leftTop.addClass('first-section');
 
 
-    } )
+
+    })
+
+    btnBack.click( function () {
+
+        leftTop.addClass('left-top-move');
+        leftTop.removeClass('first-section');
+
+
+
+
+    })
 
 })
